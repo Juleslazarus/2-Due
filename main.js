9ec727ff23828e9ca1793fd92da2fccde2e7ff1a
Julian Naranjo R <naranjojulian702@gmail.com>
2022-09-17 17:52:17 -0700

    added more modern styles and some features on hover

--- main.js
+++ main.js
@@ -1,54 +1,89 @@
-const addItemBtn = document.querySelector('.addItemBtn'); 
+const addItemBtn = document.querySelector(".addItemBtn");
 
-const addItemInput = document.querySelector('.addItemInput'); 
+const addItemInput = document.querySelector(".addItemInput");
 
+const todoListContainer = document.querySelector(".todoListContainer");
 
-const todoListContainer = document.querySelector('.todoListContainer'); 
+const importantBtn = document.querySelector(".importantBtn");
 
-const importantBtn = document.querySelector('.importantBtn'); 
+const importantLabel = document.querySelector(".importantLabel");
 
-const importantLabel = document.querySelector('.importantLabel'); 
+const notImportantLabel = document.querySelector(".notImportantLabel");
 
-const notImportantLabel = document.querySelector('.notImportantLabel'); 
+const notImportantBtn = document.querySelector(".notImportantBtn");
 
-const notImportantBtn = document.querySelector('.notImportantBtn'); 
+importantBtn.addEventListener("click", () => {
+  if (importantBtn.checked) {
+    importantLabel.style.textDecoration = "underline";
+    notImportantLabel.style.color = "black";
+    importantLabel.style.fontWeight = "600";
+  }
+});
+notImportantBtn.addEventListener("click", () => {
+  if (notImportantBtn.checked) {
+    notImportantLabel.style.color = "#666666";
+    importantLabel.style.textDecoration = "none";
+    importantLabel.style.fontWeight = "400";
+  }
+});
 
-importantBtn.addEventListener('click', () => {
-    if(importantBtn.checked) {
-        importantLabel.style.textDecoration = 'underline';
-        notImportantLabel.style.color = 'black'; 
-        importantLabel.style.fontWeight = '600';  
+addItemBtn.addEventListener("click", () => {
+  let todo = addItemInput.value;
+
+  let todoItem = document.createElement("p");
+
+  todoItem.textContent = todo;
+
+  todoItem.classList.add("todoItem");
+
+  if (todoItem.textContent === "" || todoItem.textContent === "  ") {
+    alert("You Need To Add Text Into The Text Field");
+  } else {
+    const removeItem = document.createElement("button");
+
+    removeItem.textContent = "X";
+
+    removeItem.classList.add("removeItem");
+
+    const checkItem = document.createElement("button");
+
+    checkItem.textContent = "✔";
+
+    checkItem.classList.add("checkItem");
+
+    const uncheckItem = document.createElement("button");
+
+    uncheckItem.textContent = "+";
+
+    uncheckItem.classList.add("uncheckItem");
+
+    todoListContainer.appendChild(todoItem);
+
+    todoItem.appendChild(removeItem);
+
+    todoItem.appendChild(uncheckItem);
+
+    todoItem.appendChild(checkItem);
+
+    if (importantBtn.checked) {
+      todoItem.classList.add("important");
+      todoItem.style.fontWeight = "600";
+    } else if (notImportantBtn.checked) {
+      todoItem.classList.add("notImportant");
     }
-})
-notImportantBtn.addEventListener('click', () => {
-    if(notImportantBtn.checked) {
-        notImportantLabel.style.color = '#666666';
-        importantLabel.style.textDecoration = 'none';  
-        importantLabel.style.fontWeight = '400'; 
-    }
-})
 
-addItemBtn.addEventListener('click', () => {
-    let todo = addItemInput.value; 
+    removeItem.addEventListener("click", () => {
+      todoItem.remove();
+    });
+    checkItem.addEventListener("click", () => {
+      todoItem.style.textDecoration = "line-through";
+      todoItem.style.background = "rgb(255, 158, 158)";
+    });
+    uncheckItem.addEventListener("click", () => {
+      todoItem.style.textDecoration = "none";
+      todoItem.style.background = "white";
+    });
 
-    let todoItem = document.createElement('p');
-
-    todoItem.textContent = todo; 
-
-    todoItem.classList.add('todoItem'); 
-
-    todoListContainer.appendChild(todoItem); 
-
-    if(importantBtn.checked){
-        todoItem.classList.add('important');
-        todoItem.style.fontWeight = '600';  
-    }else if(notImportantBtn.checked) {
-        todoItem.classList.add('notImportant');
-    }
-    
-    
-    todoItem.addEventListener('mouseover', () => {
-        todoItem.style.color = 'white !important'; 
-    })
-
-})
+    addItemInput.value = "";
+  }
+});
